da229bf9625eadba3961d06cc98c653174aa7da9
aitorgit <tugasmec@gmail.com>
2018-10-24 00:13:14 +0200

    Some C.AirSpace attributes

--- 2ndJavaPractice_flySimulator/src/elemnts/AirSpace.java
+++ 2ndJavaPractice_flySimulator/src/elemnts/AirSpace.java
@@ -1,5 +1,16 @@
 package elemnts;
 
+import java.util.ArrayList;
+
 public class AirSpace {
+	
+	public static final int XMAX = 10; // Delimitacio de l'espai aeri, espai real = 10^3
+	public static final int YMAX = 10;
+	public static final int MAXCAP = 5;
+	
+	int[][] zone = new int[XMAX][YMAX]; // ini constructor
+	ArrayList<Object> currentAirplanes = new ArrayList<Object>();
+	
+	
 
 }
