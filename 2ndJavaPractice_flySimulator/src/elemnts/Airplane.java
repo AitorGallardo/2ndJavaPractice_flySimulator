4cb18655cbe589f09f481408323ec372aae083eb
aitor gallardo muñoz <gmaitor@outlook.com>
2018-10-24 00:36:33 +0200

    Added attributes+blanc constructor to C.Airplane

--- 2ndJavaPractice_flySimulator/src/elemnts/Airplane.java
+++ 2ndJavaPractice_flySimulator/src/elemnts/Airplane.java
@@ -1,5 +1,18 @@
 package elemnts;
 
 public class Airplane {
+	
+	String model, manufacturer, numberPlate;
+	int positionX, positionY;
+	double altitude;
+	double direction;
+	double speed;
+	boolean motorOn;
+	int MaxCapacity;
+	
+	public Airplane() {
+		
+	}
+	
 
 }
