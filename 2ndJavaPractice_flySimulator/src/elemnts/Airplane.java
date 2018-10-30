52678452c73104e6447593c4767370a5975f61cd
aitorgit <tugasmec@gmail.com>
2018-10-30 17:01:24 +0100

    Added airplane getters, motor,speed,altit func()

--- 2ndJavaPractice_flySimulator/src/elemnts/Airplane.java
+++ 2ndJavaPractice_flySimulator/src/elemnts/Airplane.java
@@ -21,6 +21,76 @@ public class Airplane {
 		
 		
 	}
+
+	public void turnOn_motor() {
+		motorOn = true;
+	}
+	public void turnOff_motor() {
+		motorOn = false;
+	}
+	public void increaseSpeed(int addSpeed) {
+		speed+=addSpeed;
+	}
+	public void decreaseSpeed(int reduceSpeed) {
+		speed-=reduceSpeed;
+	}
+	public void increaseAltitude() {
+		
+	}
+	public void decreaseAltitude() {
+		
+	}
+	
+	
+	
+	
+	
+	// Getters
+	
+	public String getModel() {
+		return model;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getNumberPlate() {
+		return numberPlate;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public double getAltitude() {
+		return altitude;
+	}
+
+	public double getDirection() {
+		return direction;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public boolean isMotorOn() {
+		return motorOn;
+	}
+
+	public int getMaxCapacity() {
+		return MaxCapacity;
+	}
+	
+	
+	
+
+	
 	
 
 	
